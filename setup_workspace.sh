2d5e986d080c87ce442478499942912f33788d81
Dominik Zuercher <dominikz@phys.ethz.ch>
2019-10-14 10:59:12 +0200

    Adding logging

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -12,7 +12,10 @@
 ############################################################
 
 #name of the workspace
-name='20191014_test'
+name='/cluster/home/dominikz/20191014_DR1_MCCL_MAP_COMPARISON'
+
+#author
+author="Dominik Zuercher"
 
 #git directories to clone
 git_repos=('git@cosmo-gitlab.phys.ethz.ch:cosmo/esub.git' '-b modular git@cosmo-gitlab.phys.ethz.ch:DZuercher/UStats.git' 'git@cosmo-gitlab.phys.ethz.ch:rsgier/ECl.git')
@@ -25,10 +28,52 @@ modules=('new' 'python/3.6.1' 'intel/2018.1' 'gcc/4.8.2' 'open_mpi/3.0.0')
 
 ############################################################3
 
-if [ "$1" == "initialize" ]
+function append_python_path {
+    for i in $( ls -d repos/*/ );
+    do
+        printf "Adding repository $i to PYTHONPATH variable \n"
+        export PYTHONPATH=$i:$PYTHONPATH
+    done
+}
+
+function activate_env {
+    #source virtual environement
+    printf "Activating virtual python environement ${venv} \n"
+    source ${venv}/bin/activate
+}
+
+function clone_repos {
+    #clone repositories
+    for repo in "${git_repos[@]}";
+    do
+        printf "Cloning ${repo} \n"
+        git clone $repo
+    done
+}
+
+function load_modules {
+   for module in "${modules[@]}";
+   do
+        printf "Loading module ${module} \n"
+        module load ${module}
+
+        #append modules to environement
+        if [ "$1" == "log" ];
+        then
+            printf $module >> environement
+        fi
+    done
+
+    if [ "$1" == "log" ];
+    then
+        printf "\n \n" >> environement
+    fi
+}
+
+if [ "$1" == "initialize" ];
 then
 
-    printf "Initializing workspace in ${name}"
+    printf "Initializing workspace in ${name} \n"
 
     #check if already exists
     if [ -d ${name} ];
@@ -38,78 +83,56 @@ then
     fi
 
     #create the top directory
+    printf "Creating directory ${name} \n"
     mkdir ${name}
     cd ${name}
 
     #create doc files
-    printf "Author: Dominik Zuercher" >> README
-    printf "Author: Dominik Zuercher" >> pipe 
+    printf "Creating files README, pipe and environement \n"
+    printf "Author: ${author}" >> README
+    printf "Author: ${author}" >> pipe 
 
     touch environement
 
     #Load modules on demand
-    if [ "$2" == "euler" ]
+    if [ "$2" == "euler" ];
     then
-        printf $"Modules: \n" >> environement
-        for module in "${modules[@]}";
-        do
-            module load $module
-
-            #append modules to environement
-            printf $module >> environement
-        done
-        printf "\n \n" >> environement
+        load_modules log 
     fi
 
-    #source virtual environement
-    source ${venv}/bin/activate
+    activate_env
 
     #create subdirectories
+    printf "Creating subdirectories data, source and repos \n"
     mkdir data
     mkdir source
     mkdir repos
 
-    #clone repositories
     cd repos
-    for repo in "${git_repos[@]}";
-    do
-        git clone $repo
-    done
+    clone_repos
     cd ..
 
-    #append git repositories to pythonpath
-    for i in $( ls -d repos/*/ );
-    do
-        export PYTHONPATH=$i:$PYTHONPATH
-    done
+    append_python_path
 
     printf $"pip freeze: \n" >> environement
     pip freeze >> environement
 
-elif [ "$1" == "activate" ]
+elif [ "$1" == "activate" ];
 then
 
-    printf "Activating workspace in ${name}"
+    printf "Activating workspace in ${name} \n"
 
     #Load modules on demand
-    if [ "$2" == "euler" ]
+    if [ "$2" == "euler" ];
     then
-        for module in modules;
-        do
-            module load $module
-        done 
+        load_modules
     fi
 
-    #source virtual environement
-    source ${venv}/bin/activate
+    activate_env
 
     cd ${name}
 
-    #append git repositories to pythonpath
-    for i in $( ls -d repos/*/ );
-    do
-        export PYTHONPATH=$i:$PYTHONPATH
-    done
+    append_python_path
 
 fi
 
